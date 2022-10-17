2b817650f3ea9cd59cb9051621cb99ed1ea8725b
ryry23 <zwei230@gmail.com>
2022-10-18 08:55:03 +0900

    update sql

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -28,5 +28,5 @@ CREATE TABLE `visit_history` (
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`),
-  INDEX 'mlt_idx' ('tenant_id', 'player_id', 'competition', 'created_id')
+  INDEX 'mlt_idx' ('tenant_id', 'player_id', 'competition_id', 'created_at')
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
