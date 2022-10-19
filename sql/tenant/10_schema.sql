af2b83d5cdbee7499e80da88086e1417236e1b98
ryry23 <zwei230@gmail.com>
2022-10-20 00:10:23 +0900

    インデックスを設定

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -34,5 +34,5 @@ CREATE TABLE player_score (
 create index tenant_id on competition(tenant_id);
 create index id on competition(id);
 create index player_seach on player(tenant_id, created_at);
-create index player_id on player(id);
+create index player_id on player(id);インデックスを
 create index search on player_score(tenant_id,competition_id, player_id);
